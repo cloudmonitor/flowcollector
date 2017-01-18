e2ef21e5054168fe9bed5f79e368336182658310
henulwj <henulwj@qq.com>
2017-01-18 21:02:09 +0800

    增加获取虚拟机租户名

--- flowcollector/getinstance.py
+++ flowcollector/getinstance.py
@@ -38,14 +38,42 @@ def get_hypervisor_instances_and_interface():
     headers = {"Content-type": "application/json", "X-Auth-Token": admin_token_id, "Accept": "application/json"}
     url = NOVA_ENDPOINT.format(tenant_id=admin_tenant_id)
     all_instance_info = get_all_tenant_instances()
+    all_tenants = get_all_tenants(admin_token_id)
     hostname = gethostname()
     for instance in all_instance_info['servers']:
         if instance['OS-EXT-SRV-ATTR:hypervisor_hostname'] == hostname:
             r = requests.get(url + '/servers/' + instance['id'] + "/os-interface", headers=headers)
             instance['interfaceAttachments'] = r.json()['interfaceAttachments']
+            tenant_name = _get_instance_tenant_name(instance["tenant_id"], all_tenants)
+            instance["tenant_name"] = tenant_name
             hypervisor_servers_list.append(instance)
     return hypervisor_servers_list
 
 
+def get_all_tenants(token_id):
+    """获得所有的租户信息，并进行过滤掉service项目"""
+    headers = {"Content-type": "application/json", "X-Auth-Token": token_id, "Accept": "application/json"}
+    url = "http://controller:35357/v2.0/tenants"
+    r = requests.get(url=url, headers=headers)
+    tenants = r.json()
+    del_tenants = []
+    for tenant in tenants["tenants"]:
+        if tenant["name"] == "service":
+            del_tenants.append(tenant)
+    for del_tenant in del_tenants:
+        tenants["tenants"].remove(del_tenant)
+    return tenants
+
+
+def _get_instance_tenant_name(instance_tenant_id, tenants):
+    tenant_name = "unknown"
+    for tenant in tenants["tenants"]:
+        if instance_tenant_id == tenant["id"]:
+            tenant_name = tenant["name"]
+            break
+    return tenant_name
+
+
 if __name__ == "__main__":
-    print get_hypervisor_instances_and_interface()
+    import json
+    print json.dumps(get_hypervisor_instances_and_interface())
